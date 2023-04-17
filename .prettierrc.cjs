8c4fc9929e2694290a7a2005e4fa1c2509383cbf
zk <1099571219@qq.com>
2023-04-18 00:10:53 +0800

    chore: 添加eslint commit 格式

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -18,4 +18,5 @@ module.exports = {
     vueIndentScriptAndStyle: false, //不对vue中的script及style标签缩进
     endOfLine: 'lf', //结束行形式
     embeddedLanguageFormatting: 'auto', //对引用代码进行格式化
+    trailingComma: "none"
   }
